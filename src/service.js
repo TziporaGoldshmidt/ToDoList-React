fc4f4db66c30ba8f2af32253a6b0ac18dadeb332
Ubuntu <ubuntu@ip-172-31-46-221.eu-north-1.compute.internal>
2025-06-03 14:52:44 +0000

    changes from aws

--- src/service.js
+++ src/service.js
@@ -1,21 +1,26 @@
 import axios from 'axios';
 
+// הגדרת כתובת ה-API ב-default
+const apiUrl = "/api";
+//axios.defaults.baseURL = apiUrl;
+
+
 const Service = {
   getTasks: async () => {
-    const result = await axios.get(`/tasks`)   
+    const result = await axios.get(`${apiUrl}/tasks`)   
     console.log(result.data)
     return result.data;
   },
 
   addTask: async(name)=>{
     console.log('addTask', name)
-    const result = await axios.post(`/tasks`,{name})  
+    const result = await axios.post(`${apiUrl}/tasks`,{name})  
     return result.data;
   },
 
   setCompleted: async(id, isComplete)=>{
     console.log('setCompleted', {id, isComplete})
-    const result = await axios.put(`/tasks/${id}`,{isComplete})  
+    const result = await axios.put(`${apiUrl}/tasks/${id}`,{isComplete})  
     return result.data;
   },
 
